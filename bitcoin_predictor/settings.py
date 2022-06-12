1a7d5306867fda12e2eef4d9efe18bc9d554fd01
Haardik <77044000+Haardik-Ravat@users.noreply.github.com>
2022-06-12 15:52:56 +0530

    Update settings.py

--- bitcoin_predictor/settings.py
+++ bitcoin_predictor/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$k#m$&eer#8ompp6s=1%kf^0!vi&2ksmu#%r=rtj9q#$d!g6pg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
